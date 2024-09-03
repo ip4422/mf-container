5bd203839f26643cfc1c5fd3eedb32b11ab4799e
Igor Popov <ipopov4422@gmail.com>
2024-09-03 20:44:12 +0100

    Cleanup code and pass settings to MFE app

--- src/app/mfe-apps/MfeApp1.tsx
+++ src/app/mfe-apps/MfeApp1.tsx
@@ -1,14 +1,27 @@
 import { Suspense, lazy } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 const App1 = lazy(() => import('remoteApp1/App1'))
 
 const MfeApp1: React.FC = () => {
+  const navigate = useNavigate()
+  const idTag = '_mfe-app1'
+
   return (
     <div className='Container'>
       <Suspense fallback={<div>Loading...</div>}>
-        <App1 settings={'some string from MfeApp1'} />
+        <div id={idTag}>
+          <App1
+            settings={{
+              idTag,
+              basename: '/',
+              parentHomeName: '/',
+              parentNavigate: navigate
+            }}
+          />
+        </div>
       </Suspense>
     </div>
   )
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,13 +2,6 @@ import MfeApp1 from 'app/mfe-apps/MfeApp1'
 
 export const HomePage: React.FC = () => {
   return <MfeApp1 />
-  // return (
-  //   <>
-  //     <section className='my-8'>some content</section>
-
-  //     <section className='my-8'>some content</section>
-  //   </>
-  // )
 }
 
 export default HomePage
